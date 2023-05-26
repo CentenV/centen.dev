5e1d84836d92ac895f23273a39b7795003430cf0
CentenV <vincent.nguyennf@gmail.com>
2023-05-25 21:17:18 -0600

    Preparing to move navigation bar to the bottom of the screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 import "./globals.scss";
+import HOMEPAGESTYLE from "./home.module.scss";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      home page
+      <div id={HOMEPAGESTYLE["home-page-container"]}>
+        <div id={HOMEPAGESTYLE["greeting"]}>
+          <div>
+            CentenV
+            Vincent Nguyen
+          </div>
+          <div>
+            <Image src={"/images/portrait.JPG"} alt="Portrait" width={1647} height={2203} id={HOMEPAGESTYLE["greeting-image"]} draggable={false} />
+          </div>
+        </div>
+      </div>
+      
     </div>
   );
 }
